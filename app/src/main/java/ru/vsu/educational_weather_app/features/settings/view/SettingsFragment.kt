e2a2f30b1328f5a07144c95681b240cf7fd4e259
Ушаков Владимир <eau120676@gmail.com>
2024-11-18 12:09:44 +0300

    removed unused imports

--- app/src/main/java/ru/vsu/educational_weather_app/features/settings/view/SettingsFragment.kt
+++ app/src/main/java/ru/vsu/educational_weather_app/features/settings/view/SettingsFragment.kt
@@ -5,16 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.appbar.MaterialToolbar
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import ru.vsu.educational_weather_app.R
-import ru.vsu.educational_weather_app.databinding.FragmentMainWeatherBinding
 import ru.vsu.educational_weather_app.databinding.FragmentSettingsBinding
-import ru.vsu.educational_weather_app.features.weather.view.MainWeatherVM
 
 class SettingsFragment : Fragment() {
     val vm: SettingsVM by viewModel()
